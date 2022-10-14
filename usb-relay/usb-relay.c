2a175ba02f239773cb63be1bcd75ba4af10b71f9
hackboxguy <albert.david@gmail.com>
2022-10-14 23:16:23 +0200

    code cleanup of usb-relay.c in openwrt-base-scripts package

--- usb-relay/usb-relay.c
+++ usb-relay/usb-relay.c
@@ -1,4 +1,12 @@
-//disclaimer: code snippet is from: https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/#full-example
+
+//this utility sends uart bytes for controlling "LC Technology" usb relay board
+//for on  ==> 0xA0,0x01,0x01,0xA2 will be sent @9600 baud on /dev/ttyUSBx node.
+//for off ==> 0xA0,0x01,0x00,0xA1 will be sent @9600 baud on /dev/ttyUSBx node.
+//https://github.com/andrewintw/usb-powered-relay
+//datasheet: https://images.100y.com.tw/pdf_file/57-LCUS-1.pdf
+
+//disclaimer: code snippet for uart communication is taken from:
+//https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/#full-example
 #include <stdio.h>   /* Standard input/output definitions */
 #include <string.h>  /* String function definitions */
 #include <unistd.h>  /* UNIX standard function definitions */
@@ -10,6 +18,8 @@
 #include <signal.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+#define DEFAULT_SERIAL_PORT "/dev/ttyUSB0"
 /*****************************************************************************/
 int open_port(char *serial_node)
 {
@@ -24,7 +34,7 @@ int open_port(char *serial_node)
 /*****************************************************************************/
 int exit_serial_port(int fd,struct termios* old_options)
 {
-    if (tcsetattr(fd, TCSANOW, old_options) != 0) 
+    if (tcsetattr(fd, TCSANOW, old_options) != 0)
     {
 		printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
 		return -1;
@@ -75,63 +85,71 @@ int init_serial_port(int fd, struct termios* old_options,int baudrate)
 void usage(const char *program)
 {
     printf("Usage: %s ", program);
-    printf("[-d <dev_node>] [-n <relay_pos>] [-s <on/off>] ");
+    printf("[-d <dev_node>] [-n <relay_pos>] [-s <on/off>]\n");
     printf("    -d - usb-relay-device-node\n");
     printf("    -n - relay position\n");
     printf("    -s - relay state [on/off]\n");
     printf("    e.g. %s -d /dev/ttyUSB0 -n 1 -s on\n",program);
-    //exit(EXIT_FAILURE);
 }
 /*****************************************************************************/
 int main(int argc, char *argv[])
 {
-	struct termios old_options;	
+	struct termios old_options;
 	unsigned char buffer[16];
-    const char* program = "usb-relay-ctl";//basename(argv[0]);
-    char dev_node[256];
-    char relay_state[256];
-    int relay_pos;
-    int opt = 0;
-    while ((opt = getopt(argc, argv, "d:n:s")) != -1)
-    {
-        switch(opt)
-        {
-            case 'd':
-                strcpy(dev_node,optarg);
-                break;
-            case 'n':
-                relay_pos = atoi(optarg);
-                break;
-            case 's':
-                strcpy(relay_state,optarg);
-                break;
-            default:
-                usage(program);
-                break;
-        }
-    }
+	char dev_node[256];
+	char relay_state[256];unsigned char state_flag=0;
+	int relay_pos;unsigned char pos_flag=0;
+	int opt = 0;
+	strcpy(dev_node,DEFAULT_SERIAL_PORT);
+	while ((opt = getopt(argc, argv, "d:n:s:")) != -1)
+	{
+		switch(opt)
+		{
+			case 'd':
+				strcpy(dev_node,optarg);
+				break;
+			case 'n':
+				pos_flag=1;
+				relay_pos = atoi(optarg);
+				break;
+			case 's':
+				state_flag=1;
+				strcpy(relay_state,optarg);
+				break;
+			default:
+				usage(argv[0]);//program);
+				break;
+		}
+	}
 
+	if(pos_flag ==0 || state_flag ==0)
+	{
+		printf("-n and -s options must specified\n");
+		usage(argv[0]);
+		return -1;
+	}
 	buffer[0]=0XA0;
 	buffer[1]=0x01;
-    if(strcmp(relay_state,"on"))
-    {	
-        buffer[2]=0x01;	
-        buffer[3]=0xA2;	
-    }
-    else
-    {
-        buffer[2]=0x00;	
-        buffer[3]=0xA1;	
-    }
-
-	int serial_fd=open_port("/dev/ttyUSB0");	
+	if(strcmp(relay_state,"on"))
+	{
+		buffer[2]=0x01;
+		buffer[3]=0xA2;
+	}
+	else //off
+	{
+		buffer[2]=0x00;
+		buffer[3]=0xA1;
+	}
+	int serial_fd=open_port(dev_node);
 	if(serial_fd == -1 )
-        return -1;
+        {
+		printf("unable to open to %s\n",dev_node);
+		return -1;
+	}
 	init_serial_port(serial_fd,&old_options,B9600);
-	write(serial_fd,buffer,4);	
-    exit_serial_port(serial_fd,&old_options);
-  	
-    close(serial_fd);	
+	write(serial_fd,buffer,4);
+	exit_serial_port(serial_fd,&old_options);
+	close(serial_fd);
 	return 0;
 }
 /*****************************************************************************/
