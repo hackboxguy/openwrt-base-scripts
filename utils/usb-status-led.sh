0ecfef5ac50e481d28576ed034ad7ec5ec3c448e
hackboxguy <albert.david@gmail.com>
2022-10-24 14:23:57 +0200

    added standard powerstate json format support for usb-status-led.sh script

--- utils/usb-status-led.sh
+++ utils/usb-status-led.sh
@@ -42,18 +42,32 @@ if [ -z $1 ]; then
 		echo "{\"ledstate\":\"unknown\"}"
 		;;
 	esac
+	return 0
 fi
 
 if [ -f $1 ]; then
         #check if argument $1 is a json file
         JQOBJ=$(jq type < $1)
         if [ $? = "0" ]; then #this is a valid json file
-                MYARG=$(jq -r .ledstate $1)
+		MYARG=$(jq -r .ledstate $1)
+		#also accept { powerstate: on}
+		if [ $MYARG = "null" ]; then
+			MYARG=$(jq -r .powerstate $1)
+			if [ $MYARG = "null" ]; then
+				echo "invalid json arguments!!"
+				return 1
+			fi
+		fi
         else
                 MYARG=$1 #just take argument as color string
         fi
 else
         MYARG=$1
+fi
+
+#also accept { powerstate: on}
+if [ $MYARG = "on" ]; then
+	MYARG=white
 fi
 
 blinkone-tool --$MYARG > /dev/null
