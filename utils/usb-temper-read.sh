7eaacde0cc6bc119cce3c97c4942dd6dd25918ab
hackboxguy <albert.david@gmail.com>
2024-03-22 14:20:22 +0100

    added option to print raw value for usb-temper-read.sh script

--- utils/usb-temper-read.sh
+++ utils/usb-temper-read.sh
@@ -3,4 +3,9 @@
 TEMPER=$(tempered)
 VAL=$(echo $TEMPER | awk '{print $4}')
 UNIT=$(echo $TEMPER | awk '{print $5}')
-echo "{ \"temperature\" : \"$VAL\"}"
+#if raw value is requested then print it, else print in json format
+if [ "$1" = "-r" ]; then 
+	echo "$VAL"
+else
+	echo "{ \"temperature\" : \"$VAL\"}"
+fi
